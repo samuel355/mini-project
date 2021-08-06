807c04454429f1cd418c557a31def59ee9a812d5
Samuel Osei Adu <asamuel355@yahoo.com>
2021-08-07 00:46:13 +0100

    new update

---
+++ .history/auto_20210806235046.php
@@ -0,0 +1,120 @@
+<!DOCTYPE html>
+<html lang="en">
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>Pick & Drop</title>
+  <meta developer="Samuel Osei Adu" name="The developer">
+  <meta author="Nhyira Kojo Dey" name="Almighty">
+
+  <!-- Favicons -->
+  <link href="assets/img/logo.png" rel="icon">
+  <link href="assets/img/logo.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+    <script defer
+            src="https://maps.googleapis.com/maps/api/js?libraries=places&language=<?= $_SESSION['lang'] ?>&key=AIzaSyC3jQOcOFgfWrdqFWXvCl3RzsyS_c1DYmY"
+            type="text/javascript">
+    </script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+
+</head>
+
+<body>
+
+<div class="container-fluid">
+    <!-- Language -->
+
+    <div class="row">
+        <div class="col-sm-12">
+            <div id="map" style="height: 500px; width: 100%" ></div>
+        </div>
+    </div> <br>
+
+    <div class="text-center"><a href="index.php" class="btn btn-info">Return Home</a></div> <hr>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-8">
+                <!-- form -->
+                <h4 class="text-center font-weight-bolder">Drop Item For Pickup</h4>
+                <form id="distance_form">
+
+                    <div class="form-group" style="margin-top: 1rem;">
+                        <label class="text-dark"> Pick Up Location : </label>
+                        <input class="form-control" id="from_places" placeholder="<?= $lang['enter_location'] ?>" required />
+                        <input id="origin" name="origin" required="" type="hidden"/>
+                    </div>
+
+                    <div class="form-group"><label> Destination : </label>
+                        <input class="form-control" id="to_places" placeholder="<?= $lang['enter_location'] ?>" required />
+                        <input id="destination" name="destination" required="" type="hidden"/>
+                    </div>
+
+                    <div class="form-group" style="display: none;">
+                        <label><?= $lang['travel_mode'] ?></label>
+                        <select class="form-control" id="travel_mode" name="travel_mode">
+                            <option value="DRIVING"><?= $lang['Driving'] ?></option>
+                        </select>
+                    </div>
+                    <div class="form-group">
+                        <label for="item detail"> Item Details :</label>
+                        <input type="text" class="form-control" required placeholder="item details">
+                    </div>
+                    <div class="form-group">
+                        <label for="quantity">Size : </label>
+                        <select class="form-control" name="size" id="size">
+                            <option value="1">1kg</option>
+                            <option value="2">2kg</option>
+                            <option value="3">3kg</option>
+                            <option value="4">4kg</option>
+                            <option value="5">5kg</option>
+                        </select>
+                    </div>
+                    <div class="form-group">
+                        <label for="receiver-details" class="text-dark"> Receiver's Details: </label>
+                        <textarea name="receiver-details" id="receiver-details" class="form-control" cols="30" rows="10" required> </textarea>
+                    </div> <br>
+
+                    <div class="text-center">
+                        <button role="button" class="btn btn-primary text-center" type="submit" style="padding: 10px; font-weight: bold;"> <?= $lang['calculate_btn'] ?> </button>
+                    </div><br>
+
+                </form>
+            </div>
+
+            <!-- result -->
+            <div class="col-sm-4" style="margin-top: 2rem;">
+                <div id="result" class="hide">
+                    <ul class="list-group">
+                        <li id="in_mile" class="list-group-item d-flex justify-content-between align-items-center"></li> <br>
+                        <br>
+                        <br>
+                        <li id="in_kilo" class="list-group-item d-flex justify-content-between align-items-center"></li> <br>
+                        <br>
+                        <br>
+
+                        <li id="duration_text" class="list-group-item d-flex justify-content-between align-items-center"></li> <br>
+                        <br>
+                        <li id="price" class="list-group-item d-flex justify-content-between align-items-center"></li> <br>
+                        <form action="">
+                            <div class="text-center">
+                                <button class="btn btn-dark">Proceed to Payment</button>
+                            </div>
+                        </form>
+                    </ul>
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+</div>
+
+<?php include('./includes/script.php') ?>
+
+</body>
+
+</html>
---
+++ .history/client/client-profile_20210807002940.php
@@ -0,0 +1,211 @@
+<?php include('./includes/head.php') ?>
+
+    <body>
+		<!-- Main Wrapper -->
+        <div class="main-wrapper">
+		
+			<!-- Header -->
+            <?php include('./includes/header.php') ?>
+			<!-- /Header -->
+			
+			<!-- Sidebar -->
+            <?php include('./includes/sidebar.php') ?>
+			<!-- /Sidebar -->
+			
+			<!-- Page Wrapper -->
+            <div class="page-wrapper">
+			
+				<!-- Page Content -->
+                <div class="content container-fluid">
+				
+					<!-- Page Header -->
+					<div class="page-header">
+						<div class="row">
+							<div class="col-sm-12">
+								<h3 class="page-title">Profile</h3>
+								<ul class="breadcrumb">
+									<li class="breadcrumb-item"><a href="index.html">Dashboard</a></li>
+									<li class="breadcrumb-item active">Profile</li>
+								</ul>
+							</div>
+						</div>
+					</div>
+					<!-- /Page Header -->
+					
+					<div class="card mb-0">
+						<div class="card-body">
+							<div class="row">
+								<div class="col-md-12">
+									<div class="profile-view">
+										<div class="profile-img-wrap">
+											<div class="profile-img">
+												<a href="">
+													<img  src="<?php echo (!empty($row['photo'])) ? './assets/img/profiles/'.$row['photo'] : './assets/img/profiles/avatar-19.jpg'; ?>" alt="User Image">
+												</a>
+											</div>
+										</div>
+										<div class="profile-basic">
+											<div class="row">
+												<div class="col-md-5">
+													<div class="profile-info-left">
+														<h3 class="user-name m-t-0"><?php echo $row['firstname']. " " .$row['lastname']; ?></h3>
+														<div class="staff-msg"><a href="#" class="btn btn-custom" data-toggle="modal" data-target="#update-details">Edit profile</a></div>
+													</div>
+												</div>
+												<div class="col-md-7">
+													<ul class="personal-info">
+														<li>
+															<span class="title">Phone:</span>
+															<span class="text"><a href="#"> <?php echo (!empty($row['phone'])) ? $row['phone'] : 'Update your phone number'; ?> </a></span>
+														</li>
+														<li>
+															<span class="title">Email:</span>
+															<span class="text"><a href="#"><?php echo (!empty($row['email'])) ? $row['email'] : 'Update your phone Email'; ?></a></span>
+														</li>
+														<li>
+															<span class="title">Joined:</span>
+															<span class="text"><?php echo $row['registered_on'] ?></span>
+														</li>
+														<li>
+															<span class="title">Address:</span>
+															<span class="text"><?php echo $row['user_address'] ?></span>
+														</li>
+													</ul>
+													<div class="staff-msg"><a href="#" class="btn btn-custom" data-toggle="modal" data-target="#current-location">set your exact location</a></div>
+												</div>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+							
+						</div>
+					</div>
+
+					<div class="container">
+						<div class="row">
+							<div class="col-md-12">
+								<div class="modal-content">
+									<div class="modal-header">
+										<h5>Set Exact location so we could locate you with ease</h5>
+									</div>
+									
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<!-- /Page Content -->
+
+				<!-- Update Modal-->
+				<div id="update-details" class="modal custom-modal fade" role="dialog">
+					<div class="modal-dialog modal-dialog-centered modal-lg" role="document">
+						<div class="modal-content">
+							<div class="modal-header">
+								<h5 class="modal-title">Update Your Details</h5>
+								<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+									<span aria-hidden="true">&times;</span>
+								</button>
+							</div>
+							<div class="modal-body">
+
+							<!--notifications-->
+							<div class="row">
+								<div class="col-12">
+
+									<div id="process" class="alert alert-primary alert-dismissible fade show form-group input-group" role="alert" style="display: none;">
+										<strong>Processing ! .... </strong>
+										<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+									</div>
+
+									<div id="alert" class="alert alert-danger alert-dismissible fade show form-group input-group" role="alert" style="display: none;">
+										<strong id="messages"></strong>
+										<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+									</div>
+
+									<div id="succ" class="alert alert-info alert-dismissible fade show form-group input-group" role="alert" style="display: none;">
+										<strong id="done"></strong>
+										<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+									</div>
+								</div>
+							</div>
+              				<!--/notifications-->
+
+								<form role="form" action="includes/update.php" method="POST">
+									<div class="row">
+										<div class="col-md-6">
+											<div class="form-group">
+												<label class="col-form-label">First Name <span class="text-danger">*</span></label>
+												<input class="form-control" name="firstname" id="firstname" value="<?php echo $row['firstname']?>" type="text">
+											</div>
+										</div>
+										<div class="col-md-6">
+											<div class="form-group">
+												<label class="col-form-label">Last Name</label>
+												<input class="form-control" name="lastname" id="lastname" value="<?php echo $row['lastname']?>" type="text">
+											</div>
+										</div>
+										<div class="col-md-6">
+											<div class="form-group">
+												<label class="col-form-label">Photo <span class="text-danger">*</span></label>
+												<input class="form-control" name="user_image" id="user_image" type="file">
+											</div>
+										</div>
+										<div class="col-md-6">
+											<div class="form-group">
+												<label class="col-form-label">Email <span class="text-danger">*</span></label>
+												<input class="form-control floating" value="<?php echo $row['email']?>" name="email" id="email" type="email" readonly>
+											</div>
+										</div>
+										<div class="col-md-6">
+											<div class="form-group">
+												<label class="col-form-label">Address</label>
+												<input class="form-control" name="user_address" id="user_address" value="<?php echo $row['user_address']?>" type="text">
+											</div>
+										</div>
+										<div class="col-md-6">
+											<div class="form-group">
+												<label class="col-form-label">Phone</label>
+												<input class="form-control" value="<?php echo $row['phone']?>" id="phone" name="phone" type="text">
+											</div>
+										</div>
+										
+									</div>
+									<div class="submit-section">
+										<button id="submit" name="save-details" type="submit" class="btn btn-primary submit-btn">Submit</button>
+									</div>
+								</form>
+							</div>
+						</div>
+					</div>
+				</div>
+
+
+				<!-- Address Map-->
+				<div id="current-location" class="modal custom-modal fade" role="dialog">
+					<div class="modal-dialog modal-dialog-centered modal-lg" role="document">
+						<div class="modal-content">
+							<div class="modal-header">
+								<h5 class="modal-title">Update Your Details</h5>
+								<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+									<span aria-hidden="true">&times;</span>
+								</button>
+							</div>
+							<div class="modal-body">
+								<input
+									id="pac-input"
+									class="controls"
+									type="text"
+									placeholder="Search Box"
+								/>
+								<div id="map"></div>
+							</div>
+						</div>
+					</div>
+				</div>
+				
+            </div>
+        </div>
+		<!-- /Main Wrapper -->
+		
+		<?php include('./includes/scripts.php') ?>
